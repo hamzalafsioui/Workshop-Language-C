b627edb5097f97a203180d30a3827c1deb5b62f5
hamzalafsioui <hamzalafsioui@gmail.com>
2025-09-02 06:28:28 -0400

    Decimal to Hex

--- DAY 1/Variables/Challenge-13.c
+++ DAY 1/Variables/Challenge-13.c
@@ -6,16 +6,44 @@
 // Challenge 13 : Affichage des valeurs binaire et hexadécimale équivalentes
 // Écrivez un programme C pour afficher la valeur équivalente en binaire et en hexadécimal. Entrez un nombre entier et affichez ses valeurs équivalentes en binaire et en hexadécimal.
 
+void decimalToHexadecimal(int n) {
+    char hexDigits[] = "0123456789ABCDEF";
+    
+    
+    char hex[20]; // Max 20 digits
+    
+    int i = 0;
+    
+    // Check if the number is 0
+    if (n == 0) {
+        printf("0");
+        return;
+    }
+
+    
+    while (n > 0) {
+        hex[i] = hexDigits[n % 16]; // Store remainder
+        n = n / 16; // last loop n will be 0 and forloop  stop here
+        i++;
+    }
+
+    // Print reverse order
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%c", hex[j]);
+    }
+}
+
 
 
 int main (){
 
     printf("\n\n\n");
 
-    int numberEntier = 6;
+    int numberEntier = 25;
 
+
+    // decimal to binary
     int mcd = 32;
-
     while (mcd >=0)
     {
     
@@ -23,6 +51,12 @@ int main (){
         mcd--;
         
     }
+
+    printf("\n");
+
+    // decimal to Hexa
+
+decimalToHexadecimal(numberEntier);
     
 
 
