772091f3fed1efb92dae327f0aa9a1ca3d107a2c
hamzalafsioui <hamzalafsioui@gmail.com>
2025-09-03 05:10:50 -0400

    Implement challenge-08 Fibonacci

---
+++ DAY 1/Boucles/challenge-08.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// Challenge 8 : Affichage de la Suite de Fibonacci
+// Écrivez un programme C qui génère les n premiers termes de la suite de Fibonacci, où n est entré par l’utilisateur. 
+// La suite de Fibonacci est définie comme suit : F(0) = 0, F(1) = 1, et F(n) = F(n-1) + F(n-2).
+
+int main (){
+
+    int n;
+    printf("entré combien de nombre qui vous faire génère: ");
+    scanf("%d",&n);
+
+    int a =0, b=1;
+    int c;
+
+    printf("%d %d",a,b);
+    for (int i = 0; i < n; i++)
+    {
+        c = a + b;
+        printf(" %d",c);
+        a = b;
+        b = c;
+        
+    }
+    
+    
+
+
+    return 0;
+}
