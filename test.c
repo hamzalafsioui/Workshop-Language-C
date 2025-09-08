9c08dee4654d28f608e15583e99ce10fe989d811
hamzalafsioui <hamzalafsioui@gmail.com>
2025-09-08 15:31:43 +0100

    Implement Dir of DAY4

--- test.c
+++ test.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
+
 int main() {
-    printf("\n\nHello, World!\n");
+    
+    int arr[] = {1, 1, 2, 2, 2, 3, 3, 3, 3};
+    int size = (sizeof(arr)/sizeof(arr[0]));
+    int maxLength = 0,currentLength = 0, maxNumber = 0;
+    for (int i = 1; i < size; i++)
+    {
+        if(arr[i] == arr[i-1]){
+            currentLength++;
+        }else{
+            if(currentLength>maxLength){
+                maxLength = currentLength;
+                maxNumber = arr[i-1];
+            }
+            
+            currentLength = 1;
+        }
+
+    }
+    if(currentLength>maxLength){
+        maxLength = currentLength;
+        maxNumber = arr[size-1];
+    }
+
+
+    printf("NUmber: %d repeat: %d",maxNumber,maxLength);
+    
+
     return 0;
 }
